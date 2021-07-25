39cd8fa89a2584eaa6b71256c4cdd587cd6def03
Shikha Singh <32593425+SHIKHASINGH1506@users.noreply.github.com>
2021-07-25 11:20:58 +0530

    functionality completed

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 var translateButton = document.querySelector("#btn-translate");
-var textArea = document.querySelector("#txt-area");
+var inputText = document.querySelector("#input-text");
+var outputArea = document.querySelector("#output");
+var minionTranslateUrl = "https://api.funtranslations.com/translate/minion.json";
 
-function eventListener(){
-    console.log("clicked");
-    console.log(textArea.value);
+function getTranslationURL(text){
+    return encodeURI(minionTranslateUrl+ "?" + "text="+ text);
 }
 
+function eventListener(){
+    var input = inputText.value; //taking input
+    //server is processing 
+    fetch(getTranslationURL(input))
+    .then( response => response.json())
+    .then(  json => {
+        var output = json.contents.translated;
+        outputArea.innerText = output;//output
+        console.log("converted text", output)
+    })
+    .catch( error => console.log("Error occured", error));
+}
+
+//To better understand fetch() 
+    // fetch(getTranslationURL(inputText.value))
+    // .then(function parseResultToJson(response){
+    //     response.json();
+    // })
+    // .then(function printResponse(json) // 
+    // {
+    //     console.log(json);
+    // })
+
 translateButton.addEventListener("click", eventListener);
+//Exercise 04 challenge
+// document.querySelector("textarea")
+// document.querySelector(".btn-primary");
+// document.querySelector("#input-btn")
+// document.querySelector("input[name = 'translator']")
